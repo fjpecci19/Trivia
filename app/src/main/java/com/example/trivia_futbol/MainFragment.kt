c7081e18688dafd8c6e8d89f857c8c84fac39d4c
[Franco <[fapeccisan@gmail.com]>
2023-08-01 12:58:01 -0400

    Avanzando

---
+++ app/src/main/java/com/example/trivia_futbol/MainFragment.kt
@@ -0,0 +1,34 @@
+package com.example.trivia_futbol
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.trivia_futbol.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+
+        binding.first.setOnClickListener {
+            val firstFragment = FirstFragment()
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.main_navigation_container, firstFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+
+        }
+
+        return binding.root
+    }
+
+}
